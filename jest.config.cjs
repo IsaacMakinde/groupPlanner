900de69a5c39aa9bd766303e076e89853dd43ce1
Isaac <isaac.makinde3@mail.dcu.ie>
2024-05-18 21:05:28 +0100

    added jest and sample tests

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  bail: 1,
+  verbose: true,
+  testPathIgnorePatterns: ["<rootDir>/.github", "<rootDir>/node_modules"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  testEnvironment: "jest-environment-jsdom",
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+};
