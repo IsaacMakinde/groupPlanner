91c966fd96fecaf2de4b1ed261e6d60b36ef682c
Isaac <isaac.makinde3@mail.dcu.ie>
2024-05-19 19:58:42 +0100

    setup routing

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+const Home: React.FC = () => {
+  return (
+    <div>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Welcome to the Home page</h1>
+          <p className="subtitle">
+            My first website with <strong>Bulma</strong>!
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
