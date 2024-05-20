d3fe21da567ace3f26adc7db9833b20eee597d1f
Isaac <isaac.makinde3@mail.dcu.ie>
2024-05-20 01:02:18 +0100

    added components help create an event

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+import Button from "../components/ui/Button";
+import EventForm from "../components/form/EventForm";
+
 const Home: React.FC = () => {
+  const [showForm, setShowForm] = useState(false);
+  const handleEventCreateButton = () => {
+    console.log("Create Event!");
+    // trigger a form to be displayed
+    setShowForm((showForm) => !showForm);
+  };
+
   return (
     <div>
       <section className="section">
         <div className="container">
-          <h1 className="title">Welcome to the Home page</h1>
-          <p className="subtitle">
-            My first website with <strong>Bulma</strong>!
-          </p>
+          <h1 className="title is-1">Welcome to the Home page</h1>
+          <Button
+            onClick={handleEventCreateButton}
+            label="createEventButton"
+            styling={"button is-primary"}
+          ></Button>
         </div>
+        <EventForm
+          showForm={showForm}
+          onClose={handleEventCreateButton}
+        ></EventForm>
       </section>
     </div>
   );
